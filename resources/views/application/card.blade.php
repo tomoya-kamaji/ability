403eaf426f2441b66dc88615049e1388b4724e06
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-02-14 07:55:01 +0900

    レビュー投稿画面の修正

--- resources/views/application/card.blade.php
+++ resources/views/application/card.blade.php
@@ -1,33 +1,17 @@
-{{-- <div class="card deck col-md-10 center mx-auto">
-    <div class="row">
-        <div class="col-md-2 d-flex align-items-center">
-            <img src={{ $result['artworkUrl512']}} class="d-block mx-auto" width="100" height="100">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">{{ $result['trackName']}}</h5>
-                <p class="card-text">{{ $result->pivot['content']}}</p>
-            </div>
-        </div>
-    </div>
-</div> --}}
-
-
-
 <div class="card border-0">
     <div class="row no-gutters">
         <div class="col-md-4 my-auto">
-            <img class="d-block mx-auto" src={{ $result['artworkUrl512']}} width="150" height="150">
+            <img class="d-block mx-auto" src={{ $application['artworkUrl512']}} width="150" height="150">
         </div>
         <div class="col-md-8">
             <div class="card-body">
                 <div class="row">
-                    <h5 class="card-title col-md-9">{{ $result['trackName']}}</h5>
-                    <a href="{{ route('application.edit', ['id' => $result['trackId']]) }}" class="btn btn-primary col-md-3">レビュー</a>
+                    <h5 class="card-title col-md-9">{{ $application['trackName']}}</h5>
+                    <a href="{{ route('application.edit', ['id' => $application['trackId']]) }}" class="btn btn-primary col-md-3">レビュー</a>
                 </div>
                 <hr color="#797979">
-                <p>ここにはアプリの説明が入ります</p>
-                <p>アプリの詳細について</p>
+                <p style="display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden;">{{ $application['description'] }}    </p>
+                <p>レビューの１件目</p>
             </div>
         </div>
     </div>
--- resources/views/application/edit.blade.php
+++ resources/views/application/edit.blade.php
@@ -1,38 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<?php $result = $posts['results'][0]; ?>
+<?php $application = $applications['results'][0]; ?>
+
+
+
+<div class="jumbotron p-2" style="background:url(images/bimg001.jpg); background-color: #3e68c9">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-2">
+                <img class="d-block mx-auto img-thumbnail" src={{ $application['artworkUrl512']}} width="150" height="150">
+            </div>
+            <div class="col-md-8 d-flex align-items-center">
+                <h2 class="text-white">「{{ $application['trackName'] }}」のレビュー投稿</h2>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="card col-md-5">
-            <div class="card-body">
-                @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-                @endif
-                <div class="card">
-                    <img class="card-img-top" src={{ $result['artworkUrl512'] }} alt="写真">
-                    <title>{{ $result['trackName'] }}</title>
-
-                    <div class="card-body" style="background-color:#e4f0f2">
-                        <h5 class="card-title">{{ $result['trackName'] }}</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-1">
+        <div class="col-md-3">
+            <h2>レビュー</h2>
 
         </div>
-        <div class="col-md-6">
+        <div class="col-md-9">
             <form method="POST" action="{{ route('application.register') }}">
                 @csrf
-                <div class="form-group">
-                    <label>レビュー</label>
-                    <textarea class="form-control" name="content" id="exampleFormControlTextarea1" rows="3" placeholder="ex)使いやすい"></textarea>
-                    <small class="form-text text-muted">マイページにて変更可能です。</small>
-                </div>
+                <h3>
+                    <ins>あなたが利用する「{{ $application['trackName'] }}」についてお答えください</ins><br>
+                </h3>
+
                 <div class="form-group">
                     <label for="exampleFormControlSelect1">オススメ度</label>
                     <select class="form-control" name='evaluation' id="exampleFormControlSelect1">
@@ -43,11 +41,22 @@
                         <option value="1">1：オススメできない</option>
                     </select>
                 </div>
+                <div class="form-group">
+                    <label>レビュータイトル</label>
+                    <input class="form-control input-lg" name="content" id="title" rows="3" placeholder="ex)使いやすい">
+                    <small class="form-text text-muted">※必須</small>
+                </div>
+                <div class="form-group">
+                    <label>レビュー</label>
+                    <textarea class="form-control" name="content" id="exampleFormControlTextarea1" rows="3" placeholder="ex)使いやすい"></textarea>
+                    <small class="form-text text-muted">マイページにて変更可能です。</small>
+                </div>
+
                 <input type="hidden" name="user_id" value={{ $user -> id }}></p>
-                <input type="hidden" name="trackId" value={{ $result['trackId'] }}></p>
-                <input type="hidden" name="artworkUrl512" value={{ $result['artworkUrl512'] }}></p>
-                <input type="hidden" name="artistName" value={{ $result['artistName'] }}></p>
-                <input type="hidden" name="trackName" value={{ $result['trackName'] }}></p>
+                <input type="hidden" name="trackId" value={{ $application['trackId'] }}></p>
+                <input type="hidden" name="artworkUrl512" value={{ $application['artworkUrl512'] }}></p>
+                <input type="hidden" name="artistName" value={{ $application['artistName'] }}></p>
+                <input type="hidden" name="trackName" value={{ $application['trackName'] }}></p>
                 <button type="submit" class="btn btn-primary">登録</button>
             </form>
         </div>
