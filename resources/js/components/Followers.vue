effb26d3697b204153ede193a21a708dff828a0e
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-02-28 09:55:30 +0900

    #57 フォローフォロワー一覧の雛形完成

---
+++ resources/js/components/Followers.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="row">
+        <div class="col-md-2 d-flex align-items-center">
+            画像イメージ
+            <!-- <img
+            :src="artworkURL512"
+            class="shadow d-block mx-auto"
+            width="100"
+            height="100"
+            /> -->
+        </div>
+        <div class="col-md-2">
+            <h3 class="mt-4 mb-0">{{ name }}</h3>
+        </div>
+        <div class="col-md-6">
+            <h5 class="mb-1">職業{{ profession }}</h5>
+            <p class="text-secondary mt-2 mb-0">年代{{ age }}才</p>
+        </div>
+        <div class="col-md-2">
+            <button type="button" class="btn btn-danger m-4">フォロワー</button>
+        </div>
+        </div>
+        <hr color="#797979">
+    </div>
+</template>
+
+<script>
+import StarRating from 'vue-star-rating'
+
+export default {
+  components: { StarRating },
+  props: {
+    id: Number,
+    name: String,
+    age: String,
+    email: String,
+    pivot: Object,
+    email_verified_at: String,
+    profession: String,
+    created_at: String,
+    updated_at: String,
+  },
+
+  data() {
+    return {
+    };
+  },
+
+
+};
+</script>
--- resources/js/components/LikeApplications.vue
+++ resources/js/components/LikeApplications.vue
@@ -1,36 +1,36 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-md-2 d-flex align-items-center">
-        <img
-          :src="artworkURL512"
-          class="shadow d-block mx-auto"
-          width="100"
-          height="100"
-        />
-      </div>
-      <div class="col-md-2">
-        <h3 class="mt-4 mb-0">{{ trackName }}</h3>
-        <p>{{ artistName }}</p>
-        <StarRating
-            :read-only="true"
-            :rating= 3
-            :increment="0.5"
-            :max-rating="5"
-            :star-size="20">
-        </StarRating>
-      </div>
-      <div class="col-md-6">
-          <p class="text-secondary mt-2 mb-0">2021/02/18</p>
-          <h5 class="mb-1"></h5>
-          <p class="text-secondary" style="display: -webkit-box; -webkit-line-clamp: 4; -webkit-box-orient: vertical; overflow: hidden;">ここにはアプリの説明文を入れる。ここにはアプリの説明文を入れる。ここにはアプリの説明文を入れる</p>
-      </div>
-      <div class="col-md-2">
-            <button type="button" class="btn btn-danger m-4">　　削除　　</button>
-      </div>
+        <div class="col-md-2 d-flex align-items-center">
+            <img
+            :src="artworkURL512"
+            class="shadow d-block mx-auto"
+            width="100"
+            height="100"
+            />
+        </div>
+        <div class="col-md-2">
+            <h3 class="mt-4 mb-0">{{ trackName }}</h3>
+            <p>{{ artistName }}</p>
+            <StarRating
+                :read-only="true"
+                :rating= 3
+                :increment="0.5"
+                :max-rating="5"
+                :star-size="20">
+            </StarRating>
+        </div>
+        <div class="col-md-6">
+            <p class="text-secondary mt-2 mb-0">2021/02/18</p>
+            <h5 class="mb-1"></h5>
+            <p class="text-secondary" style="display: -webkit-box; -webkit-line-clamp: 4; -webkit-box-orient: vertical; overflow: hidden;">ここにはアプリの説明文を入れる。ここにはアプリの説明文を入れる。ここにはアプリの説明文を入れる</p>
+        </div>
+        <div class="col-md-2">
+                <button type="button" class="btn btn-danger m-4">　　削除　　</button>
+        </div>
+        </div>
+        <hr color="#797979">
     </div>
-    <hr color="#797979">
-</div>
 </template>
 
 <script>
