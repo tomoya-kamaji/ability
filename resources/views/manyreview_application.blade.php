3ca0ce4420443d625b51c7bdcff5509050ebe03a
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-03-03 21:21:23 +0900

    Home画面の編集

--- resources/views/manyreview_application.blade.php
+++ resources/views/manyreview_application.blade.php
@@ -11,8 +11,8 @@
         @elseif ($i == 3)
             <i class="fas fa-crown" aria-hidden="true">　No.4</i>
         @endif
-        <a href="{{ route('application.detailpage', ['id' => $recent_application->trackId]) }}"><img class="d-block mx-auto img-thumbnail m-3 shadow-sm" src={{ $manyreview_application->artworkURL512}} width="125" height="125"></a>
-        <a href="{{ route('application.detailpage', ['id' => $recent_application->trackId]) }}">
+        <a href="{{ route('application.detailpage', ['id' => $manyreview_application->trackId]) }}"><img class="d-block mx-auto img-thumbnail m-3 shadow-sm" src={{ $manyreview_application->artworkURL512}} width="125" height="125"></a>
+        <a href="{{ route('application.detailpage', ['id' => $manyreview_application->trackId]) }}">
             <h5 class="card-title text-center" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden;">{{ $manyreview_application->trackName}}</h5>
         </a>
         <i class="far fa-comment-dots text-center m-0">{{ $manyreview_application->application_count}}件</i>
