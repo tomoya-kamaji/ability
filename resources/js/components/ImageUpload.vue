3c00ac37de2c240f9bb8d08cbee77e753df02b7d
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-03-06 23:13:54 +0900

    #83 写真アップロード機能

--- resources/js/components/ImageUpload.vue
+++ resources/js/components/ImageUpload.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-    <Upload />
+    <upload v-model="picture" />
   </div>
 </template>
 
 <script>
-import Upload from './Upload.vue'
+import Upload from './ImageaUpload.vue'
 export default {
     components: {
         Upload
+    },
+    data() {
+        return {
+            picture: null
+        }
     }
-
 }
 </script>
