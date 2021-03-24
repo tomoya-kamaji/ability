b70e19b6d170c8935003305fa48cb9dad55f8908
tomo_kamaji <tomoya.kamaji@gmail.com>
2021-03-24 11:25:59 +0900

    #106 CircleCiコマンドパス変更

--- ability/resources/views/footer.blade.php
+++ ability/resources/views/footer.blade.php
@@ -19,7 +19,7 @@
                     <dl>
                         <a class="text-white" href="{{ route('home')}}"><dd>ホーム</dd></a>
                         @guest
-                        a
+                            自動デプロイOK
                         @else
                             <a class="text-white" href="{{ route("users.show", ["name" => Auth::user()->name]) }}"><dd>マイページ</dd></a>
                         @endguest
